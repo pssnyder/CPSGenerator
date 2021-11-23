85351a9fd2c611fec7fcdeb127436bc7cd50b0a0
Patrick Snyder <2072916+pssnyder@users.noreply.github.com>
2021-11-23 09:58:56 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import csv
+
+# read
+with open('eggs.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
+
+# write
+with open('eggs.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
